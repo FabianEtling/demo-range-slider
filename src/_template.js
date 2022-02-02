467a0fc6715ef0fcdbcacc8c6f296dd6724d5a8a
FabianEtling <fabian.etling@cedis.fu-berlin.de>
2022-02-02 17:16:38 +0100

    feat: add alternative slider template

--- src/_template.js
+++ src/_template.js
@@ -29,19 +29,16 @@ export default {
             <input id="form_low" class="demo-slider__input input-number" type="number" name="form_low"> </input>
             <input id="form_high" class="demo-slider__input input-number" type="number" name="form_high"> </input>
           </div>
-
-          <!-- Material Design Layout -->
-          <div class="demo-slider__track">
-            <div class="demo-slider__track--inactive"></div>
-            <div class="demo-slider__track--active">
-              <div class="demo-slider__track--active_fill"></div>
-            </div>
+        </div>
+      </template>
+      <template data-layout="simple">
+        <div id="wrapper" class="demo-slider demo-range-slider">
+          <div id="sliders">
+            <input id="slider_low" class="demo-slider__input input-range" type="range" min="0" max="50" name="slider_low"><input id="slider_high" class="demo-slider__input input-range" type="range" min="50" max="100" name="slider_high">
           </div>
-          <div class="demo-slider__thumb">
-            <div class="demo-slider__thumb-knob"></div>
-          </div>
-          <div class="demo-slider__thumb">
-            <div class="demo-slider__thumb-knob"></div>
+          <div id="input-forms" style="display:none">
+            <input id="form_low" class="demo-slider__input input-number" type="number" name="form_low"> </input>
+            <input id="form_high" class="demo-slider__input input-number" type="number" name="form_high"> </input>
           </div>
         </div>
       </template>
